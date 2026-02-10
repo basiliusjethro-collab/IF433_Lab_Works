7855d4254971d4c76714939bc23909a82c1a035a
Basilius Jethro <basilius.jethro@student.umn.ac.id>
2026-02-10 12:59:22 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_BasiliusJethro_57833/week01/GameStore.kt
+++ src/oop_BasiliusJethro_57833/week01/GameStore.kt
@@ -3,4 +3,10 @@ package oop_BasiliusJethro_57833.week01
 fun main() {
     val gameTitle = "Elden Ring"
     val price = 600000
+
+    val finalPrice = calculateDiscount(price)
 }
+
+fun calculateDiscount(price: Int) =
+    if (price > 500000) (price * 80) / 100
+    else (price * 90) / 100
