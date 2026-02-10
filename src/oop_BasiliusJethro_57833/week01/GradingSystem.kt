4fab0a77bae9d7c592724b96dd78424b6d072b2e
Basilius Jethro <basilius.jethro@student.umn.ac.id>
2026-02-10 12:17:40 +0700

    week01: refactor to use val and string templates

--- src/oop_BasiliusJethro_57833/week01/GradingSystem.kt
+++ src/oop_BasiliusJethro_57833/week01/GradingSystem.kt
@@ -1,9 +1,9 @@
 package oop_BasiliusJethro_57833.week01
 
 fun main () {
-    var name: String = "Basilius Jethro"
+    val name: String = "Basilius Jethro"
     var score: Int = 80
 
 
-    println("Nama: " + name + " , Nilai: " + score)
+    println("Nama: $name , Nilai: $score")
 }
