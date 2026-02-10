c0090dcf8e95bc67690b5bf9efea18dabeb555c3
Basilius Jethro <basilius.jethro@student.umn.ac.id>
2026-02-10 12:10:01 +0700

    week01: initial setup and procedural code

---
+++ src/oop_BasiliusJethro_57833/week01/GradingSystem.kt
@@ -0,0 +1,9 @@
+package oop_BasiliusJethro_57833.week01
+
+fun main () {
+    var name: String = "Basilius Jethro"
+    var score: Int = 80
+
+
+    println("Nama: " + name + " , Nilai: " + score)
+}
