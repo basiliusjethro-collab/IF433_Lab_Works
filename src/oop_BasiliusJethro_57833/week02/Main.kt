6a114d598d6defebf231b059a79adc8b7d1e9b5d
Basilius Jethro <basilius.jethro@student.umn.ac.id>
2026-02-11 13:44:21 +0700

    week02: (task 1) implement library input system

--- src/oop_BasiliusJethro_57833/week02/Main.kt
+++ src/oop_BasiliusJethro_57833/week02/Main.kt
@@ -3,6 +3,10 @@ package oop_BasiliusJethro_57833.week02
 import java.util.Scanner
 
 fun main() {
+    runStudentSystem()
+    //runLoanSystem()
+}
+fun runStudentSystem() {
     val scanner = Scanner(System.`in`)
 
     print("Masukkan Nama: ")
@@ -14,6 +18,7 @@ fun main() {
     println("Pilih Jalur:")
     println("1. Dengan Jurusan")
     println("2. Tanpa Jurusan")
+
     val choice = scanner.nextInt()
     scanner.nextLine()
 
@@ -24,4 +29,25 @@ fun main() {
     } else {
         Student(name, nim)
     }
+}
+
+fun runLoanSystem() {
+    val scanner = Scanner(System.`in`)
+
+    println("=== LIBRARY SYSTEM ===")
+
+    print("Judul Buku: ")
+    val title = scanner.nextLine()
+
+    print("Nama Peminjam: ")
+    val borrower = scanner.nextLine()
+
+    print("Lama Pinjam (hari): ")
+    var duration = scanner.nextInt()
+
+    if (duration < 0) duration = 1
+
+    val loan = Loan(title, borrower, duration)
+
+    println("Total Denda: Rp ${loan.calculateFine()}")
 }
