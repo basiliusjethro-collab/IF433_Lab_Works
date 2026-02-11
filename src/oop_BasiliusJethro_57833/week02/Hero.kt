8ec0a1722ac887cefed42ec115db7f8a82a3ee78
Basilius Jethro <basilius.jethro@student.umn.ac.id>
2026-02-11 13:46:29 +0700

    week02: (task 2) create Hero class

---
+++ src/oop_BasiliusJethro_57833/week02/Hero.kt
@@ -0,0 +1,19 @@
+package oop_BasiliusJethro_57833.week02
+
+class Hero(
+    val name: String,
+    val baseDamage: Int,
+    var hp: Int = 100
+) {
+
+    fun attack(targetName: String) {
+        println("$name menyerang $targetName!")
+    }
+
+    fun takeDamage(damage: Int) {
+        hp -= damage
+        if (hp < 0) hp = 0
+    }
+
+    fun isAlive(): Boolean = hp > 0
+}
