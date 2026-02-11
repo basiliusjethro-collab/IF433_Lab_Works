d1043dc53f3f446ba85cba5805b0b5a3f1ecb36f
Basilius Jethro <basilius.jethro@student.umn.ac.id>
2026-02-11 13:54:05 +0700

    week02: (task 2) implement the game

--- src/oop_BasiliusJethro_57833/week02/Main.kt
+++ src/oop_BasiliusJethro_57833/week02/Main.kt
@@ -4,7 +4,8 @@ import java.util.Scanner
 
 fun main() {
     runStudentSystem()
-    //runLoanSystem()
+    runLoanSystem()
+    runRpgSystem()
 }
 fun runStudentSystem() {
     val scanner = Scanner(System.`in`)
@@ -50,4 +51,48 @@ fun runLoanSystem() {
     val loan = Loan(title, borrower, duration)
 
     println("Total Denda: Rp ${loan.calculateFine()}")
+}
+
+fun runRpgSystem() {
+    val scanner = Scanner(System.`in`)
+
+    print("Masukkan Nama Hero: ")
+    val heroName = scanner.nextLine()
+
+    print("Masukkan Base Damage: ")
+    val damage = scanner.nextInt()
+    scanner.nextLine()
+
+    val hero = Hero(heroName, damage)
+    var enemyHp = 100
+
+    while (hero.isAlive() && enemyHp > 0) {
+
+        println("1. Serang")
+        println("2. Kabur")
+        val choice = scanner.nextInt()
+
+        if (choice == 1) {
+            hero.attack("Enemy")
+            enemyHp -= hero.baseDamage
+            if (enemyHp < 0) enemyHp = 0
+
+            println("HP Enemy: $enemyHp")
+
+            if (enemyHp > 0) {
+                val enemyDamage = (10..20).random()
+                println("Enemy menyerang balik sebesar $enemyDamage!")
+                hero.takeDamage(enemyDamage)
+                println("HP Hero: ${hero.hp}")
+            }
+        } else {
+            println("Hero kabur!")
+            break
+        }
+    }
+
+    if (hero.hp > enemyHp)
+        println("Hero Menang!")
+    else
+        println("Enemy Menang!")
 }
