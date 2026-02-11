57def3f0c4c480900854865d3b550bb4f3734ed3
Basilius Jethro <basilius.jethro@student.umn.ac.id>
2026-02-11 13:30:22 +0700

    week02: (task 1) create Loan class with default args

---
+++ src/oop_BasiliusJethro_57833/week02/Loan.kt
@@ -0,0 +1,14 @@
+package oop_BasiliusJethro_57833.week02
+
+class Loan(
+    val bookTitle: String,
+    val borrower: String,
+    val loanDuration: Int = 1
+) {
+
+    fun calculateFine(): Int =
+        if (loanDuration > 3)
+            (loanDuration - 3) * 2000
+        else
+            0
+}
