d54280f600b9e791f58f92497de44fc042b6cfd3
Basilius Jethro <basilius.jethro@student.umn.ac.id>
2026-02-18 10:21:13 +0700

    week03: fix setter using backing field

--- src/oop_BasiliusJethro_57833/week03/Employee.kt
+++ src/oop_BasiliusJethro_57833/week03/Employee.kt
@@ -1,5 +1,16 @@
 package oop_BasiliusJethro_57833.week03
 
-class Employee(val name: String) {
-    var salary: Int = 0
+class Employee(
+    val name: String,
+    salary: Int
+) {
+
+    var salary: Int = salary
+        set(value) {
+            if (value >= 0) {
+                field = value
+            } else {
+                println("Salary cannot be negative!")
+            }
+        }
 }
--- src/oop_BasiliusJethro_57833/week03/Main.kt
+++ src/oop_BasiliusJethro_57833/week03/Main.kt
@@ -1,2 +1,7 @@
 package oop_BasiliusJethro_57833.week03
 
+fun main() {
+    val e = Employee("Budi")
+    println("--- Test Error ---")
+    e.salary = 5000
+}
