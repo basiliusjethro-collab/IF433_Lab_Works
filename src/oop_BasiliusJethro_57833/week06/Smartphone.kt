1049ead731c247750b5c651e404260409e9777c9
Basilius Jethro <basilius.jethro@student.umn.ac.id>
2026-03-11 10:30:30 +0700

    week06: fix diamond problem using explicit super delegation

--- src/oop_BasiliusJethro_57833/week06/Smartphone.kt
+++ src/oop_BasiliusJethro_57833/week06/Smartphone.kt
@@ -1,6 +1,10 @@
 package oop_BasiliusJethro_57833.week06
 
-// ERROR: Class Smartphone inherits multiple implementations of turnOn()
- class Smartphone : Camera, Phone {
-     
- }
+class Smartphone : Camera, Phone {
+    // Manually override to resolve ambiguity
+    override fun turnOn() {
+        super<Camera>.turnOn() // Menjalankan logika Camera
+        super<Phone>.turnOn() // Menjalankan logika Phone
+        println("Sistem operasi Smartphone berhasil booting.")
+    }
+}
