029c94bf638adbe168492c9ae7791c052237cf36
Basilius Jethro <basilius.jethro@student.umn.ac.id>
2026-03-11 10:22:39 +0700

    week06: create Watch abstract class

---
+++ src/oop_BasiliusJethro_57833/week06/Watch.kt
@@ -0,0 +1,9 @@
+package oop_BasiliusJethro_57833.week06
+
+abstract class Watch {
+    abstract fun showTime()
+
+    fun windUp() {
+        println("Memutar kenop untuk mengisi daya...")
+    }
+}
