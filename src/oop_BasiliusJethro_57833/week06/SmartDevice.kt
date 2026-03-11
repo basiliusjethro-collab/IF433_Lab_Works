85dba8f5acdcac30c56a9da45594e775c1bd2ceb
Basilius Jethro <basilius.jethro@student.umn.ac.id>
2026-03-11 10:36:53 +0700

    week06: (task) create SmartLamp

---
+++ src/oop_BasiliusJethro_57833/week06/SmartDevice.kt
@@ -0,0 +1,11 @@
+package oop_BasiliusJethro_57833.week06
+
+class SmartLamp(override val id: String, override val name: String) : SmartDevice, Switchable {
+    override fun turnOn() {
+        println("Lampu $name ($id) menyala terang.")
+    }
+
+    override fun turnOff() {
+        println("Lampu $name ($id) mati.")
+    }
+}
