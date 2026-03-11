3200f528e53e1464a1e4a81f0de11ff5466de4d5
Basilius Jethro <basilius.jethro@student.umn.ac.id>
2026-03-11 10:28:21 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_BasiliusJethro_57833/week06/GadgetInterface.kt
@@ -0,0 +1,13 @@
+package oop_BasiliusJethro_57833.week06
+
+interface Camera {
+    fun turnOn() {
+        println("Lensa kamera terbuka dan sensor aktif.")
+    }
+}
+
+interface Phone {
+    fun turnOn() {
+        println("Sinyal seluler mencari jaringan.")
+    }
+}
