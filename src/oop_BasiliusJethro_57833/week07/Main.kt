63bb5c4d8a40959b63bebfa7b6e834b0b0a46ce3
Basilius Jethro <basilius.jethro@student.umn.ac.id>
2026-03-25 11:58:47 +0700

    week07: implement data class for structural equality

--- src/oop_BasiliusJethro_57833/week07/Main.kt
+++ src/oop_BasiliusJethro_57833/week07/Main.kt
@@ -10,8 +10,8 @@ fun main() {
     client.connect()
 
     println("\n=== TEST REGULAR CLASS ===")
-    val reg1 = RegularUser("Alice", 22)
-    val reg2 = RegularUser("Alice", 22)
-    println(reg1)
-    println("Sama? ${reg1 == reg2}")
+    val data1 = RegularUser("Alice", 22)
+    val data2 = RegularUser("Alice", 22)
+    println(data1)
+    println("Sama? ${data1 == data2}")
 }
