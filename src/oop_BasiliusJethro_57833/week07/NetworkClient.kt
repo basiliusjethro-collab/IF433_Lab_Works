a3d9beaea5620c917b6d55b598e050d7482c5129
Basilius Jethro <basilius.jethro@student.umn.ac.id>
2026-03-25 11:46:23 +0700

    week07: implement companion object as factory in NetworkClient

--- src/oop_BasiliusJethro_57833/week07/NetworkClient.kt
+++ src/oop_BasiliusJethro_57833/week07/NetworkClient.kt
@@ -1,6 +1,16 @@
 package oop_BasiliusJethro_57833.week07
 
 class NetworkClient private constructor(val url: String) {
+
+    companion object {
+        const val BASE_URL = "https://www.column.ac.id/api/" //Shared constant
+
+        fun createCLient(): NetworkClient {
+            println("Membangun NetworkClient dengan BASE_URL: $BASE_URL")
+            return NetworkClient(BASE_URL)
+        }
+    }
+
     fun connect() {
         println("Connecting to $url...")
     }
