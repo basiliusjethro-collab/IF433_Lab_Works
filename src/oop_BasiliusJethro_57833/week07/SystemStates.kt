d460a4c54f96236809a7fe6cc7b7579deef6b416
Basilius Jethro <basilius.jethro@student.umn.ac.id>
2026-03-25 12:18:04 +0700

    week07: (trial) trigger non-exhaustive when compiler error

--- src/oop_BasiliusJethro_57833/week07/SystemStates.kt
+++ src/oop_BasiliusJethro_57833/week07/SystemStates.kt
@@ -4,7 +4,7 @@ enum class AppState {
     STARTING, RUNNING, STOPPED
 
     sealed class ApiResponse
-        data class Success(val data: AppState) : ApiResponse()
+        data class Success(val data: String) : ApiResponse()
         data class Error(val message: String) : ApiResponse()
         object Loading : ApiResponse()
 }
