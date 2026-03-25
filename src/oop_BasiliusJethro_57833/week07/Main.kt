9848fdab8cd43d137ab27c9de18a6e09e6045d75
Basilius Jethro <basilius.jethro@student.umn.ac.id>
2026-03-25 11:51:42 +0700

    week07: test Singleton and Companion Object in main

--- src/oop_BasiliusJethro_57833/week07/Main.kt
+++ src/oop_BasiliusJethro_57833/week07/Main.kt
@@ -1,5 +1,11 @@
 package oop_BasiliusJethro_57833.week07
 
 fun main() {
-    val client = NetworkClient("https://api.umn.ac.id")
+    println("=== TEST SINGLETON ===")
+    println("Status: ${DatabaseManager.connectionStatus}")
+    DatabaseManager.connect()
+
+    println("\n=== TEST COMPANION OBJECT ===")
+    val client = NetworkClient.createCLient() // Instansiasi lewat Factory
+    client.connect()
 }
