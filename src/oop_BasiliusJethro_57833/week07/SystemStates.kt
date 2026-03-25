17aa818b5239be00c23b6edce771d09393376d41
Basilius Jethro <basilius.jethro@student.umn.ac.id>
2026-03-25 12:07:25 +0700

    week07: create ApiResponse sealed class

--- src/oop_BasiliusJethro_57833/week07/SystemStates.kt
+++ src/oop_BasiliusJethro_57833/week07/SystemStates.kt
@@ -2,4 +2,9 @@ package oop_BasiliusJethro_57833.week07
 
 enum class AppState {
     STARTING, RUNNING, STOPPED
+
+    sealed class ApiResponse
+        data class Success(val data: AppState) : ApiResponse()
+        data class Error(val message: String) : ApiResponse()
+        object Loading : ApiResponse()
 }
