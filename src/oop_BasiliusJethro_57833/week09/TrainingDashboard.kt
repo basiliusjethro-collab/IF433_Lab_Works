4f09248e799ece3651636f7acd8407224dc29fc9
Basilius Jethro <basilius.jethro@student.umn.ac.id>
2026-04-22 10:07:04 +0700

    week09: (task) populate mock trade history list

---
+++ src/oop_BasiliusJethro_57833/week09/TrainingDashboard.kt
@@ -0,0 +1,12 @@
+package oop_BasiliusJethro_57833.week09
+
+fun main() {
+    val tradeHistory = listOf(
+        TradeLog("BTCUSDT", "LONG", 20, 15.5, "CLOSED"),
+        TradeLog("ETHUSDT", "SHORT", 10, -5.2, "CLOSED"),
+        TradeLog("BTCUSDT", "LONG", 15, 8.0, "OPEN"),
+        TradeLog("ETHUSDT", "LONG", 5, 3.5, "CLOSED"),
+        TradeLog("XRPUSDT", "SHORT", 8, -10.0, "CLOSED"),
+        TradeLog("BTCUSDT", "SHORT", 12, 0.0, "OPEN")
+    )
+}
