9aca94d0101b3f4434f933aff48de5c968d50267
Basilius Jethro <basilius.jethro@student.umn.ac.id>
2026-04-29 09:48:00 +0700

    week10: (task) populate coin repository

--- src/oop_BasiliusJethro_57833/week10/CryptoDashboard.kt
+++ src/oop_BasiliusJethro_57833/week10/CryptoDashboard.kt
@@ -2,4 +2,7 @@ package oop_BasiliusJethro_57833.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+    coinRepo.add(Coin("BTC", 1.5))
+    coinRepo.add(Coin("ETH", 10.0))
+    coinRepo.add(Coin("USDT", 1000.0))
 }
