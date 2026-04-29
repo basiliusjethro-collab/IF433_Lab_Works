accd742b91dc2fb40169b8057ed6eee58a19a62a
Basilius Jethro <basilius.jethro@student.umn.ac.id>
2026-04-29 09:35:22 +0700

    week10: instantiate Box with Int and String types

---
+++ src/oop_BasiliusJethro_57833/week10/Main.kt
@@ -0,0 +1,9 @@
+package oop_BasiliusJethro_57833.week10
+
+fun main() {
+    val intBox = Box(10)
+    val stringBox = Box("Hello")
+
+    println(intBox.getItem())
+    println(stringBox.getItem())
+}
