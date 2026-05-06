49cefb8b85524c5cc1804de675747f2abc3610ce
Basilius Jethro <basilius.jethro@student.umn.ac.id>
2026-05-06 09:00:23 +0700

    week11: (task) create SmartDevice data class

---
+++ src/oop_BasiliusJethro_57833/week11/SmartHomeModels.kt
@@ -0,0 +1,8 @@
+package oop_BasiliusJethro_57833.week11
+
+data class SmartDevice(
+    var name: String,
+    var category: String,
+    var isOnline: Boolean = false,
+    var powerLoad: Int = 0
+)
