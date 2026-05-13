86b02628eaf04caec7ca481d65ba02c4ea04b167
Basilius Jethro <basilius.jethro@student.umn.ac.id>
2026-05-13 07:54:41 +0700

    week12: implement throw keyword for validation

--- src/oop_BasiliusJethro_57833/week12/CustomException.kt
+++ src/oop_BasiliusJethro_57833/week12/CustomException.kt
@@ -1,3 +1,15 @@
 package oop_BasiliusJethro_57833.week12
 
 class InsufficientFundsException(message: String) : Exception(message)
+
+class BankAccount(var balance: Int) {
+
+    fun withdraw(amount: Int) {
+        if (amount > balance) {
+            throw InsufficientFundsException("Saldo tidak cukup!")
+        }
+
+        balance -= amount
+        println("Berhasil tarik uang. Sisa saldo: $balance")
+    }
+}
