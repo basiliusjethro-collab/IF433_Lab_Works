8db32475914c4622816f895b661d9c4da20e03af
Basilius Jethro <basilius.jethro@student.umn.ac.id>
2026-05-13 08:04:26 +0700

    week12: (task) handle feeding success using onSuccess

--- src/oop_BasiliusJethro_57833/week12/SmartFeeder.kt
+++ src/oop_BasiliusJethro_57833/week12/SmartFeeder.kt
@@ -59,4 +59,10 @@ fun main() {
         )
 
     }
+        .onSuccess { newStock ->
+
+            currentKibbleStock = newStock
+
+            println("Makan sore sukses! Sisa stok kibble: $currentKibbleStock gr")
+        }
 }
